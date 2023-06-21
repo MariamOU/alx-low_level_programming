caae4669f1e00d9ad8fa3ca48a4269aed26ba38d
MariamOU <marymyara@gmail.com>
2023-06-21 15:19:36 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,21 +1,21 @@
 #include<stdio.h>
-/**                                                                                                                                                         
- * main - the entry function                                                                                                                                
- * Description: 'Program uses putchar'                                                                                                                      
- * Return: Alway 0 (SUCCESS)                                                                                                                                
+/**
+ * main - the entry function
+ * Description: 'Program uses putchar'
+ * Return: Alway 0 (SUCCESS)
  */
 int main(void)
 {
 char c;
 
-for (c = 'a'; c <= 'z'; c++)
+for (c = 0; c <= 10; c++)
 {
-putchar(c);
-}
-for (c = 'A'; c <= 'Z'; c++)
+putchar('0' + c);
+for (c = 'A'; c <= 'E'; c++)
 {
 putchar(c);
 }
 putchar('\n');
+}
 return (0);
 }
