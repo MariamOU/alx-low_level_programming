f9b98b8ba0f1e38064baa2439e568ab23680b3e9
MariamOU <marymyara@gmail.com>
2023-06-21 11:04:40 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - the entry function
+ * Description: 'Program uses putchar'
+ * Return: Alway 0 (SUCCESS)
+ */
+int main(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+for (c = 'A'; c <= 'Z'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
