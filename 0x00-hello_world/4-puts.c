b1fa9ecbf190d58dc43d39c6b2cb5e4f9accbee2
MariamOU <marymyara@gmail.com>
2023-06-20 18:36:48 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
+//This is a program that uses the function puts and return always 0
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
+  puts("\"Programming is like building a multilingual puzzle\n");
   return (0);
 }
+
