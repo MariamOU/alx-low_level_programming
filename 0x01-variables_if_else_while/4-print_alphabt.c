436e066c78c01b708bd5475d55145206096ad183
MariamOU <marymyara@gmail.com>
2023-06-21 12:21:26 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - the entry function
+ * Description: 'Program uses putchar'
+ * Return: Alway 0 (SUCCESS)
+ */
+int main(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+{
+if (c != 'e' && c != 'q')
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
