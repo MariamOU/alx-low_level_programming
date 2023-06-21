2ea2bf91cb4979540ac7d8f42acb0275d78c495c
MariamOU <marymyara@gmail.com>
2023-06-21 07:35:18 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- * main: the entry function
+ * main - the entry function
  * Description: 'Program uses fprintf'
  * Return: 1 (Fail)
  */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
