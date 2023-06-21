ae17c25c5539c978f3bfc14a6ab9d3c837528b87
MariamOU <marymyara@gmail.com>
2023-06-21 15:39:15 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,9 +8,13 @@ int main(void)
 {
 char c;
 
-for (c = 0; c <= 15; c++)
+for (c = 0; c <= 9; c++)
 {
 putchar('0' + c);
+}
+for (c = 'a'; c <= 'f'; c++)
+{
+putchar(c);
 }
 putchar('\n');
 return (0);
