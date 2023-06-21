6add95252926647194b75da5259684709ff9e471
MariamOU <marymyara@gmail.com>
2023-06-21 16:47:34 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+/**
+ * main - the entry function
+ * Description: 'Program uses putchar'
+ * Return: Alway 0 (SUCCESS)
+ */
+int main(void)
+{
+int c;
+int d;
+
+for (c = 0; c <= 9; c++)
+{
+for (d = 0; d <= 9; d++)
+{
+if (c != d)
+{
+putchar('0' + c);
+putchar('0' + d);
+}
+if (c != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**                                                                                                                                                         
+ * main - the entry function                                                                                                                                
+ * Description: 'Program uses putchar'                                                                                                                      
+ * Return: Alway 0 (SUCCESS)                                                                                                                                
+ */
+int main(void)
+{
+int c;
+
+for (c = 0; c <= 9; c++)
+{
+putchar('0' + c);
+if (c != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
