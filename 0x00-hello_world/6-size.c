2740e786048766b15e99dea70e0384050f7a8919
MariamOU <marymyara@gmail.com>
2023-06-21 06:33:35 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,13 @@
 #include<stdio.h>
+int main(void)
+{
+char charType;
+int intType;
+float floatType;
+double doubleType;
+printf("The size of char is: %zu byte\n", sizeof(charType));
+printf("The size of integer is: %zu bytes\n", sizeof(intType));
+printf("The size of float is: %zu bytes\n", sizeof(floatType));
+printf("The size of double is: %zu bytes\n", sizeof(doubleType));   
+return (0);
+}
