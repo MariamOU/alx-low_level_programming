6e3ff02cf892f7fe2594df0015bdd4aee5efb598
MariamOU <marymyara@gmail.com>
2023-06-22 13:39:05 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * _islower - the entry function
+ * Description: 'Program uses _putchar'
+ * Return: Always 0 (SUCCESS)
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+else
+return (0);
+}
+_putchar('\n');
+}
