65c9479db0bc31256ef4c3fad8dec075f32b94d3
MariamOU <marymyara@gmail.com>
2023-06-22 12:50:02 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,9 +10,9 @@ void print_alphabet_x10(void)
 char i;
 int j;
 
-for (i = 'a'; i <= 'z'; i++)
+for (j = 0; j <= 9; j++)
 {
-	for (j = 0; j <= 9; j++)
+	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);
 	}
