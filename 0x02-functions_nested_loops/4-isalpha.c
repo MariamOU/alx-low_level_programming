30a2ec85b3b477c90e47386c5a0beb97df2da252
MariamOU <marymyara@gmail.com>
2023-06-22 14:11:31 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * _islower - the entry function
+ * @c: the character that is checked
+ * Description: 'Program uses _putchar'
+ * Return: Always 0 (SUCCESS)
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
