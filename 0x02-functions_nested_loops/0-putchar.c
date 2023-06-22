39f41e146f19f58173e856938282d892b18bb42b
MariamOU <marymyara@gmail.com>
2023-06-21 17:12:40 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - the entry function
+ * Description: 'Program uses printf'
+ * Return: Always 0 (SUCCESS)
+ */
+int main(void)
+{
+printf("_putchar \n");
+return (0);
+
+}
