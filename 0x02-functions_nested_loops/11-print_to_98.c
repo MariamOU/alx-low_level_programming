e271209af851c424caa06535a3fb61f11814d5ec
MariamOU <marymyara@gmail.com>
2023-06-23 04:36:22 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,37 +9,37 @@
 void print_to_98(int n)
 {
 if (n <= 98)
-{
-for (; n <= 98; n++)
-{
-if (n == 98)
-{
-printf("%d", n);
-else
-{
-printf("%d,", n);
-printf(" ");
-}
-}
-}
-}
-else if (n == 98)
-{
-printf("%d", n);
-}
-else if (n >= 98)
-{
-for (; n >= 98; n--)
-{
-if (n == 98)
-{
-printf("%d", n);
-else
-{
-printf("%d,", n);
-printf(" ");
-}
-}
-}
-}
+  {
+    for (; n <= 98; n++)
+      {
+	if (n == 98)
+	  {
+	    printf("%d", n);
+	    else
+	      {
+		printf("%d,", n);
+		printf(" ");
+	      }
+	  }
+      }
+  }
+ else if (n == 98)
+   {
+     printf("%d", n);
+   }
+ else if (n >= 98)
+   {
+     for (; n >= 98; n--)
+       {
+	 if (n == 98)
+	   {
+	     printf("%d", n);
+	     else
+	       {
+		 printf("%d,", n);
+		 printf(" ");
+	       }
+	   }
+       }
+   }
 }
