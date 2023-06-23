4527d0d18143cf053a49f3250438c67de18ed71b
MariamOU <marymyara@gmail.com>
2023-06-23 08:41:51 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,11 +11,14 @@ int d;
 
 for (c = 0; c <= 9; c++)
 {
-	for (d = c + 1; d <= 9; d++)
-	{
-	  putchar('0' + c);
-	  putchar('0' + d);
-	}
+	for (d = c + 1; d <= 9; d++
+	  {
+	    if (c != d)
+	      {
+		putchar('0' + c);
+		putchar('0' + d);
+	      }
+	  }
 		if (c != 9)
 		  {
 		    putchar(',');
