5355311adc10f535c20b0ece0503c8efa031b939
MariamOU <marymyara@gmail.com>
2023-06-23 05:42:51 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,16 +10,47 @@ void times_table(void)
 	int a;
 	
 	for (a = 0; a <= 9; a++)
-	{
+		{
 			printf("%d", a);
+			printf("\n");
+		}	
+	for (a = 0; a <= 9; a++)
+		{
 			printf("%d", a*2);
+			printf("\n");
+		}
+	for (a = 0; a <= 9; a++)
+		{
 			printf("%d", a*3);
+			printf("\n");
+		}
+	for (a = 0; a <= 9; a++)
+		{
 			printf("%d", a*4);
+			printf("\n");
+		}
+	for (a = 0; a <= 9; a++)
+		{
 			printf("%d", a*5);
+			printf("\n");
+		}
+		for (a = 0; a <= 9; a++)
+		{
 			printf("%d", a*6);
+			printf("\n");
+		}
+	for (a = 0; a <= 9; a++)
+		{
 			printf("%d", a*7);
+			printf("\n");
+		}
+	for (a = 0; a <= 9; a++)
+		{
 			printf("%d", a*8);
+			printf("\n");
+		}
+	for (a = 0; a <= 9; a++)
+		{
 			printf("%d", a*9);
-	}
-	
+		}	
 }
