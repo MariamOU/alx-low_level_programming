09b7d2fd8a22b25770903dd3e4bfeb88cd3de484
MariamOU <marymyara@gmail.com>
2023-06-22 19:07:09 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * print_last_digit - the entry function
+ * @n: the character that is checked
+ * Description: 'Program uses print_last_digit'
+ * Return: value
+ */
+int print_last_digit(int n)
+{
+return (n % 2);
+}
