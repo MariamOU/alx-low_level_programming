73a60ec7f9dbd80cccb7c10a495a961fae8875d0
MariamOU <marymyara@gmail.com>
2023-06-23 17:40:04 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /**
  * main - the entry function
- * Description: 'Program uses putchar'
+ * Description: 'Program uses putchar and prints all the different combinations of two digits'
  * Return: Alway 0 (SUCCESS)
  */
 int main(void)
@@ -10,21 +10,21 @@ int c;
 int d;
 
 for (c = 0; c <= 9; c++)
-  {
-    for (d = c; d <= 9; d++)
-      {
-	if (c != d)
-	  {
-	    putchar('0' + c);
-	    putchar('0' + d);
-	    if (c != 8 || d != 9)
-	      {
-		putchar(',');
-		putchar(' ');
-	      }
-	  }
-      }
-  }
+{
+for (d = c; d <= 9; d++)
+{
+if (c != d)
+{
+putchar('0' + c);
+putchar('0' + d);
+if (c != 8 || d != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
 putchar('\n');
 return (0);
 }
