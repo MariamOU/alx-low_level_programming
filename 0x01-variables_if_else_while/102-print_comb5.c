b3f548c5d100cf5ec4555ed5d23c07c2226a21b5
MariamOU <marymyara@gmail.com>
2023-06-23 20:08:15 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,20 +1,36 @@
 #include<stdio.h>
-/**                                                                                                                                                         
- * main - the entry function                                                                                                                                
- * Description: 'Program uses putchar'                                                                                                                      
- * Return: Alway 0 (SUCCESS)                                                                                                                                
+/**
+ * main - the entry function
+ * Description: 'Program prints differents combinations of two two digits'
+ * Return: Alway 0 (SUCCESS)
  */
 int main(void)
 {
+int a;
+int b;
 int c;
+int d;
 
+for (a = 0; a <= 9; a++)
+{
+for (b = a; b <= 9; b++)
+{
 for (c = 0; c <= 9; c++)
 {
-putchar('0' + c);
-if (c != 9)
+for (d = 0; d <= 9; d++)
 {
+if ((a != c) || (b != d))
+{
+putchar('0' + a);
+putchar('0' + b);
+putchar(' ');
+putchar('0' + c);
+putchar('0' + d);
 putchar(',');
 putchar(' ');
+}
+}
+}
 }
 }
 putchar('\n');
