5793ac8f070a838100c4dd31e202f2d7426dcdbc
MariamOU <marymyara@gmail.com>
2023-06-23 17:56:18 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,20 +1,33 @@
 #include<stdio.h>
-/**                                                                                                                                                         
- * main - the entry function                                                                                                                                
- * Description: 'Program uses putchar'                                                                                                                      
- * Return: Alway 0 (SUCCESS)                                                                                                                                
+/**
+ * main - the entry function
+ * Description: 'Program prints differents combinations of 3 digits'
+ * Return: Alway 0 (SUCCESS)
  */
 int main(void)
 {
+int a;
+int b;
 int c;
 
-for (c = 0; c <= 9; c++)
+for (a = 0; a <= 9; a++)
 {
+for (b = a; b <= 9; b++)
+{
+for (c = b; c <= 9; c++)
+{
+if ((a != b) && (a != c) && (b != c))
+{
+putchar('0' + a);
+putchar('0' + b);
 putchar('0' + c);
-if (c != 9)
+if (a != 7 || b != 8 || c != 9)
 {
 putchar(',');
 putchar(' ');
+}
+}
+}
 }
 }
 putchar('\n');
