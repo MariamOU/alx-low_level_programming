4caa09e08891064d61ff3a9fcbe4128261201740
MariamOU <marymyara@gmail.com>
2023-06-25 09:19:24 -0700

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,15 +14,57 @@ int largest_number(int a, int b, int c)
 
     if (a > b && b > c)
     {
+      if (a != b && b !=c)
+	{
         largest = a;
+	}
+      else if (a == b && b != c)
+	{
+	  largest = a;
+	  largest = b;
+	}
+      else
+	{
+	  largest = a;
+	  largest = b;
+	  largest = c;
+	}
     }
     else if (b > a && a > c)
     {
+       if (b != a && a !=c)
+        {
         largest = b;
+        }
+      else if (b == a && a != c)
+        {
+          largest = a;
+          largest = b;
+        }
+      else
+        {
+          largest = a;
+	  largest = b;
+	  largest = c;
+        }
     }
     else if (c > a && a > b)
     {
+       if (c != a && a !=b)
+        {
         largest = c;
+        }
+      else if (c == a && a != b)
+        {
+          largest = a;
+          largest = c;
+        }
+      else
+        {
+          largest = a;
+	  largest = b;
+	  largest = c;
+        }
     }
 
     return (largest);
