2b8d3cfa2160df34358f0046cff7f968effd9ef9
MariamOU <marymyara@gmail.com>
2023-06-25 11:25:33 -0700

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,41 +10,41 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+int largest;
 
-    if ((a > b && b > c) || (a > c && c > b) || (b == c && b < a))
-    {
-        largest = a;
-    }
-    else if ((b > a && a > c) || (b > c && c > a) || (a == c && a < b))
-    {
-        largest = b;
-    }
-    else if ((c > a && a > b) || (c > b && b > a) || (a == b && a < c))
-    {
-        largest = c;
-    }
-    else if (a == b && a > c)
-      {
-	largest = a;
-	largest = b;
-      }
-    else if (a == c && a > b)
-      {
-	largest = a;
-	largest = c;
-      }
-    else if (b == c && b > a)
-      {
-	largest = b;
-	largest = c;
-      }
-    else
-      {
-	largest = a;
-	largest = b;
-	largest = c;
-      }
+if ((a > b && b > c) || (a > c && c > b) || (b == c && b < a))
+{
+largest = a;
+}
+else if ((b > a && a > c) || (b > c && c > a) || (a == c && a < b))
+{
+largest = b;
+}
+else if ((c > a && a > b) || (c > b && b > a) || (a == b && a < c))
+{
+largest = c;
+}
+else if (a == b && a > c)
+{
+largest = a;
+largest = b;
+}
+else if (a == c && a > b)
+{
+largest = a;
+largest = c;
+}
+else if (b == c && b > a)
+{
+largest = b;
+largest = c;
+}
+else
+{
+largest = a;
+largest = b;
+largest = c;
+}
 
-    return (largest);
+return (largest);
 }
