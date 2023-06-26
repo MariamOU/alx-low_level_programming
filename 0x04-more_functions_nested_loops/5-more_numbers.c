273ba4d5e218c7c7ab221dc8354265c3e977bfc4
MariamOU <marymyara@gmail.com>
2023-06-25 18:16:38 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * more_numbers - the entry function
+ * Description: 'Program uses the function _putchar to print numbers (0 to 14)
+ * 10 times'
+ * return : Always 0 (SUCCESS)
+ */
+void more_numbers(void)
+{
+int a;
+int b;
+
+for (b = 0; b <= 9; b++)
+{
+for (a = 0; a <= 14; a++)
+{
+_putchar('0' + a);
+}
+}
+_putchar('\n');
+}
