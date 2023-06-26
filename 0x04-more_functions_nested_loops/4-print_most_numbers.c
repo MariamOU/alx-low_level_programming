c00328c38764d57b31fb17c1531bf2254502ac85
MariamOU <marymyara@gmail.com>
2023-06-25 18:09:51 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * print_most_numbers - the entry function
+ * Description: 'Program uses the function _putchar to print numbers (0 to 9)
+ * Except 2 and 4'
+ * return : Always 0 (SUCCESS)
+ */
+void print_most_numbers(void)
+{
+int a;
+
+for (a = 0; a <= 9; a++)
+{
+if (a != 2 && a != 4)
+{
+_putchar('0' + a);
+}
+}
+_putchar('\n');
+}
+
