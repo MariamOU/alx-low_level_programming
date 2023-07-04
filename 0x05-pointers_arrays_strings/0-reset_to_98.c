324a1d7f4f20a9affb0b81c95795e77fc9adc22d
MariamOU <marymyara@gmail.com>
2023-07-04 08:35:21 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,10 +4,8 @@
  * reset_to_98 - the entry function
  * @n: the character that is checked
  * Description: 'Program uses the function _putchar'
- * Return: 0
  */
 void reset_to_98(int *n)
 {
 *n = 98;
-return (0);
 }
