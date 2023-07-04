52a726405e32a6b3dcb59ead7ea114d7dd9a01a5
MariamOU <marymyara@gmail.com>
2023-07-04 08:47:36 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * reset_to_98 - the entry function
+ * @a: the first integer
+ * @b: the second integer
+ * Description: 'Program uses the function _putchar'
+ */
+void swap_int(int *a, int *b)
+{
+  int a;
+  int b;
+  *a = b;
+  *b = a;
+}
