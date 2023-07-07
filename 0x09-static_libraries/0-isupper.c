549372e05a56cdf159997a311bdce3c1f2826c15
MariamOU <marymyara@gmail.com>
2023-07-07 12:52:36 -0700

    The creation of all the functions files

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * _isupper - the entry function
+ * @c: the character that is checked
+ * Description: 'Program uses _isupper()'
+ * Return: 0
+ */
+int _isupper(int c)
+{
+return (0);
+}
---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * _strcat - the entry function
+ * @dest: the first character that is checked
+ * @src: the second character that is checked
+ * Description: 'Program uses _strcat()'
+ * Return: 0
+ */
+char *_strcat(char *dest, char *src)
+{
+return (0);
+}
