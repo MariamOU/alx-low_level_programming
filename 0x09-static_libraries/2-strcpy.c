fcbf2395ba90e11bdc8dabd89899f5a4d1122c6c
MariamOU <marymyara@gmail.com>
2023-07-09 03:19:27 -0700

    Last update

---
+++ 0x09-static_libraries/2-strcpy.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * _strcpy - the entry function
+ * @dest: the first character that is checked
+ * @src: the second character that is checked
+ * Description: 'Program uses _strcpy()'
+ * Return: 0
+ */
+char *_strcpy(char *dest, char *src)
+{
+return (0);
+}
