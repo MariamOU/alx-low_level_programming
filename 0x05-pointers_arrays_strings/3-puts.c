ba9187abaf6b70c1d536a362b092fcfe734f5298
MariamOU <marymyara@gmail.com>
2023-07-14 15:22:51 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include<stdlib.h>
+#include "main.h"
+/**
+ * _puts - the entry function
+ * @s: the variable s
+ * Description: 'Program uses _putchar'
+ * Return: an int
+ */
+void _puts(char *s)
+{
+int i = 0;
+
+while (s[i] != '\0')
+{
+_putchar(s[i]);
+i++;
+}
