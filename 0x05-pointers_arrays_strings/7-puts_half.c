34d730076f520a32a4167d785b583273296beed6
MariamOU <marymyara@gmail.com>
2023-07-15 03:39:27 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include<stdlib.h>
+#include "main.h"
+/**
+ * puts_half - the entry function
+ * @str: the variable str
+ * Description: 'Program uses _putchar'
+ * Return: a string
+ */
+void puts_half(char *str)
+{
+int i;
+int lenght = 0;
+
+for (i = (lenght[i] - 1) / 2; i < (lenght[i] - 1); i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
