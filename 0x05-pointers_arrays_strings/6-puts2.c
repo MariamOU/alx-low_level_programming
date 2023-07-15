92015b3dea9051999bd0882297798350321edff9
MariamOU <marymyara@gmail.com>
2023-07-15 01:36:18 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include<stdlib.h>
+#include "main.h"
+/**
+ * puts2 - the entry function
+ * @str: the variable str
+ * Description: 'Program uses _putchar'
+ * Return: a string
+ */
+void puts2(char *str)
+{
+int i;
+int lenght = 0;
+
+for (i = 0; i <= str[lenght]; i++)
+{
+_putchar(s[i]);
+_putchar('\n');
+}
+}
