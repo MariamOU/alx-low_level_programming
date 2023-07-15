a4f4006e5bbe28e16b39b794c4e21fbe31ce7481
MariamOU <marymyara@gmail.com>
2023-07-15 13:03:58 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include<stdlib.h>
+#include "main.h"
+/**
+ * main - the entry function
+ * @argc: the variable argc
+ * @argv: the second variable argv
+ * Description: 'Program uses printf'
+ * Return: a string
+ */
+int main(int argc, char *argv[])
+{
+if (argc > 0)
+{
+printf("%s", arg[0]);
+}
+return (0);
+}
