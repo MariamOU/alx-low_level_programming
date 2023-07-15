ef0569d51e44f53ae12b656b0279935a699420f2
MariamOU <marymyara@gmail.com>
2023-07-15 04:33:03 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,10 +11,18 @@ void puts_half(char *str)
 {
 int i;
 int lenght = strlen(str);
+int n = (lenght - 1) / 2;
 
 for (i = lenght / 2; i < lenght; i++)
 {
 _putchar(str[i]);
+}
+if (lenght % 10 == 1)
+{
+for (i = n; i < lenght; i++)
+{
+_putchar(str[i]);
+}
 }
 _putchar('\n');
 }
