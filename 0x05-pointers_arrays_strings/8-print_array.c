0df0f32f6c2e6b3bbf59a4b9587ec228eddf201b
MariamOU <marymyara@gmail.com>
2023-07-15 05:07:08 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,19 +1,19 @@
 #include<stdlib.h>
 #include "main.h"
 /**
- * puts_half - the entry function
- * @str: the variable str
- * Description: 'Program uses _putchar'
+ * print_array - the entry function
+ * @a: the variable a
+ * @n: the second variable n
+ * Description: 'Program uses printf'
  * Return: a string
  */
 void print_array(int *a, int n)
 {
-int i;
 int arr[n];
 
-for (i = 0; i < n; i++)
+for (a = 0; a < n; a++)
 {
-printf(arr[i]);
+printf("[%d]", arr[a]);
 }
- printf("\n");
+printf("\n");
 }
