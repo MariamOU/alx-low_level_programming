37343f7ce73192575c2ca10dbcdc29e58c80aeb0
MariamOU <marymyara@gmail.com>
2023-07-15 18:56:33 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include<stdlib.h>
+#include<stdio.h>
+/**
+ * main - the entry function
+ * @argc: the variable argc
+ * @argv: the second v argv
+ * Description: 'Program uses printf'
+ * Return: a string
+ */
+int main(int argc, char *argv[])
+{
+int i;
+  
+if (argc > 0)
+{
+for (i = 1; i < argc; i++)
+{
+printf("%s", argv[i]);
+printf("\n");
+}
+}
+return (0);
+}
