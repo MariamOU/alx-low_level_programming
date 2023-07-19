ba81c47419b5840206069032558bc8f82b74a3c2
MariamOU <marymyara@gmail.com>
2023-07-18 17:31:51 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,11 +2,12 @@
 
 /**
 * main - prints thename of the file
+* Description: progran uses printf
 * Return: Always 0 (SUCCESS)
 */
 
 int main(void)
 {
-	printf("%s \n", ___FILE___);
+	printf("%s \n", __FILE__);
 	return (0);
 }
