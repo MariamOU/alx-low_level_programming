f7c3b1ff57eea6668b7968349e356c48c5ff9f4a
MariamOU <marymyara@gmail.com>
2023-07-18 18:12:56 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - The name of the structure
+* @name: first element of the structure
+* @age: second element of the structure
+* @owner: third element of the structure
+*/
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+
+};
+
+
+
+#endif /* DOG_H */
