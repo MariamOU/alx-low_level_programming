da42e8c45e66e3f18969eebeb18f491f2402d34f
MariamOU <marymyara@gmail.com>
2023-07-27 11:27:54 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -4,35 +4,23 @@
  * Description: 'Program prints differents combinations of two two digits'
  * Return: Alway 0 (SUCCESS)
  */
-int main(void)
+int main()
 {
-int a;
-int b;
-int c;
-int d;
-
-for (a = 0; a <= 9; a++)
-{
-for (b = a; b <= 9; b++)
-{
-for (c = 0; c <= 9; c++)
-{
-for (d = 0; d <= 9; d++)
-{
-if ((a != c) && (b != d))
-{
-putchar('0' + a);
-putchar('0' + b);
-putchar(' ');
-putchar('0' + c);
-putchar('0' + d);
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
-}
-putchar('\n');
+    for (int i = 0; i < 100; i++)
+      {
+        for (int j = i; j < 100; j++)
+	  {
+            putchar('0' + i / 10);
+	    putchar('0' + i % 10);
+            putchar(' ');
+            putchar('0' + j / 10);
+	    putchar('0' + j % 10);
+            if (j < 99)
+	      {
+	      putchar(',');
+	      putchar(' ');
+	      }
+	  }
+      }
 return (0);
 }
