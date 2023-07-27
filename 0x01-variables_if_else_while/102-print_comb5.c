93046d948e8dcedd47f17287135991f7f81e80d3
MariamOU <marymyara@gmail.com>
2023-07-27 12:31:04 -0700

    102-print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,9 +9,9 @@ int main(void)
 int i;
 int j;
 
-for (i = 0; i < 98; i++)
+for (i = 0; i < 99; i++)
 {
-for (j = i + 1; j < 99; j++)
+for (j = i + 1; j < 100; j++)
 {
 putchar('0' + i / 10);
 putchar('0' + i % 10);
