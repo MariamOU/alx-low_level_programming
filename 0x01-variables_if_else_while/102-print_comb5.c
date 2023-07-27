576ea2cb0144aa7c2747dc6325526e4bfb2acc29
MariamOU <marymyara@gmail.com>
2023-07-27 11:32:43 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,21 +6,24 @@
  */
 int main(void)
 {
-    for (int i = 0; i < 100; i++)
-      {
-        for (int j = i; j < 100; j++)
-	  {
-            putchar('0' + i / 10);
-	    putchar('0' + i % 10);
-            putchar(' ');
-            putchar('0' + j / 10);
-	    putchar('0' + j % 10);
-            if (j < 99)
-	      {
-	      putchar(',');
-	      putchar(' ');
-	      }
-	  }
-      }
+int i;
+int j;
+
+for (i = 0; i < 100; i++)
+{
+for (j = i; j < 100; j++)
+{
+putchar('0' + i / 10);
+putchar('0' + i % 10);
+putchar(' ');
+putchar('0' + j / 10);
+putchar('0' + j % 10);
+if (j < 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
 return (0);
 }
