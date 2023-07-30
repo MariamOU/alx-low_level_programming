4160021d366ec23f67d897aa93be59c1a6d13846
MariamOU <marymyara@gmail.com>
2023-07-30 05:08:30 -0700

    5-rev_string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -4,20 +4,27 @@
 /**
  * rev_string - the entry function
  * @s: the variable s
- * Description: 'Program uses _putchar'
+ * Description: 'Program uses rev_string'
  * Return: a string
  */
 void rev_string(char *s)
 {
 int i;
-int j;
-int lenght = 0;
-lenght = strlen(s);
+int length;
 
-for (i = 0; j = lenght - 1; i < j; i++; j--)
+if (s == NULL)
+{
+return;
+}
+length = 0;
+while (s[length] != '\0')
+{
+length++;
+}
+for (i = 0; i < length / 2; i++)
 {
 char temp = s[i];
-s[i] = s[j];
-s[j] = temp;
+s[i] = s[length - i - 1];
+s[length - i - 1] = temp;
 }
 }
