9b363dfd39c8b82e1b3f59fe91cd0498e3b99341
MariamOU <marymyara@gmail.com>
2023-07-30 07:02:02 -0700

    3-print_all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,6 +2,60 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 /**
+ * print_char - the first helper function
+ * @charac: the character passed to the function
+ * Description: 'Program uses print_char'
+ * Return: a char
+ */
+void print_char(int charac)
+{
+printf("%c", charac);
+printf(", ");
+}
+
+/**
+ * print_int - the second helper function
+ * @I_number: the character passed to the function
+ * Description: 'Program uses print_int'
+ * Return: an int
+ */
+void print_int(int I_number)
+{
+printf("%d", I_number);
+printf(", ");
+}
+
+/**
+ * print_float - the third helper function
+ * @F_number: the character passed to the function
+ * Description: 'Program uses print_float'
+ * Return: a float
+ */
+void print_float(double F_number)
+{
+printf("%f", F_number);
+printf(", ");
+}
+
+/**
+ * print_string - the fourth helper function
+ * @string: the string passed to the function
+ * Description: 'Program uses print_string'
+ * Return: a string
+ */
+void print_string(char *string)
+{
+if (string == NULL)
+{
+printf("(nil)");
+}
+else
+{
+printf("%s", string);
+}
+}
+
+/**
  * print_all - the variadic function
  * @format: list of types of arguments passed to the function
  * Description: 'Program uses print_all'
@@ -15,36 +69,22 @@ va_start(args, format);
 
 while (format[i] != '\0')
 {
-if (format[i] == 'c')
+switch (format[i])
 {
-int charac = va_arg(args, int);
-printf("%c", charac);
-printf(", ");
-}
-else if (format[i] == 'i')
-{
-int I_number = va_arg(args, int);
-printf("%d", I_number);
-printf(", ");
-}
-else if (format[i] == 'f')
-{
-double F_number = va_arg(args, double);
-printf("%f", F_number);
-printf(", ");
-}
-else if (format[i] == 's')
-{
-char *string = va_arg(args, char *);
-if (string == NULL)
-{
-printf("(nil)");
-printf(", ");
-}
-else
-{
-printf("%s", string);
-}
+case 'c':
+print_char(va_arg(args, int));
+break;
+case 'i':
+print_int(va_arg(args, int));
+break;
+case 'f':
+print_float(va_arg(args, double));
+break;
+case 's':
+print_string(va_arg(args, char *));
+break;
+default:
+break;
 }
 i++;
 }
