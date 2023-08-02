3c3ebf343dab091021aed9e92df847735b6fc3c4
MariamOU <marymyara@gmail.com>
2023-08-02 08:12:07 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -6,15 +6,16 @@
 /**
  * list_len - function that counts the number of elements in a linked list
  * @h: the pointer to the first node in the linked list
- *
- * return: the number of elements in list_t
+ * Description: 'Program uses the function list_len'
+ * return: number of elements
  */
 
 size_t list_len(const list_t *h)
 {
-size_t count = 0;
-
+size_t count;
 const list_t *current = NULL;
+
+count = 0;
 current = h;
 while (current != NULL)
 {
