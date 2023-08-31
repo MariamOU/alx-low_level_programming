c36031c946d73ad1f9cb37b5c7820bd2d2a07e34
MariamOU <marymyara@gmail.com>
2023-08-30 18:20:17 -0700

    the file 5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * _sqrt_recursion - the entry function
+ * @n: a number
+ * Description: 'Program uses the function _sqrt_recursion'
+ * Return: the natural square root of a number or -1 if n has no sr
+ */
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+if (n == 0 || n == 1)
+{
+return (n);
+}
