e4db258525ab5640f7eaa5941bec73ec9b3a38e9
MariamOU <marymyara@gmail.com>
2023-09-01 16:42:44 -0700

    creating the library libmy.a

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,11 @@
+#include<stdlib.h>
+/**
+ * _putchar - check the code
+ * @c: the character in check
+ * Description: 'Program uses _putchar function'
+ * Return: Always 0
+ */
+int _putchar(char c)
+{
+write(&c);
+}
