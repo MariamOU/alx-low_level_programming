dbc635dd78f0b5c03a599b3f5a94343994dfc86e
MariamOU <marymyara@gmail.com>
2023-09-01 18:45:06 -0700

    2-args.c file

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ * @argc: number of arguments
+ * @argv: array of strings
+ * Description: 'Program uses main function'
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+int i;
+
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+return (0);
+}
