4f669532c4774bca96548d0f11555accfe7f61db
MariamOU <marymyara@gmail.com>
2023-09-01 19:19:50 -0700

    100-change.c file not complete

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ * @argc: number of arguments
+ * @argv: array of strings
+ * Description: 'Program uses main function'
+ * Return: 0 or 1
+ */
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+{
+        printf("Error\n");
+        return (1);
+}
+}
