ec95e4c1e92283b2451bbfee554e0e5c90a3f8e0
MariamOU <marymyara@gmail.com>
2023-09-01 18:31:33 -0700

    1-args.c file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ * @argc: number of arguments
+ * @argv: array of strings
+ * Description: 'Program uses main function'
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc - 1);
+
+return (0);
+}
