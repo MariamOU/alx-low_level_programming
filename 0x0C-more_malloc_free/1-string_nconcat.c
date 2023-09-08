552ae3ce8100f2366314f5b02c7e00e0e4a29358
MariamOU <marymyara@gmail.com>
2023-09-07 17:59:16 -0700

    the file 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,27 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "main.h"
 /**
- * *_calloc - check the code
- * @nmemb: the number of elements of an array
- * @size: the size of each element
- * Description: 'Program uses *_calloc function'
+ * *string_nconcat - check the code
+ * @s1: the first string
+ * @s2: the second string
+ * @n: the first bytes of s2
+ * Description: 'Program uses *string_nconcat function'
  * Return: pointer
  */
-void *_calloc(unsigned int nmemb, unsigned int size)
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-if (nmemb == 0 || size == 0)
+unsigned int s1_len;
+unsigned int s2_len;
+char *result;
+
+if (s1 == NULL)
+{
+s1 = "";
+}
+
+if (s2 == NULL)
+{
+s2 = "";
+}
+s1_len = strlen(s1);
+s2_len = strlen(s2);
+
+if (n >= s2_len)
+{
+n = s2_len;
+}
+
+result = (char *)malloc(s1_len + n + 1);
+
+if (result == NULL)
 {
 return (NULL);
 }
 
-*ptr = malloc(nmemb * size);
+strcpy(result, s1);
+strncat(result, s2, n);
 
-if (ptr == NULL)
-{
-return (NULL);
-}
-memset(ptr, 0, nmemb * size);
-
-return (ptr);
+return (result);
 }
