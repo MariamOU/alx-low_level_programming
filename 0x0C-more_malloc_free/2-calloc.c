f9d4d5f03e7fa18ac0af5de5acc85ee414ab0c9b
MariamOU <marymyara@gmail.com>
2023-09-07 17:51:24 -0700

    the file 2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * *_calloc - check the code
+ * @nmemb: the number of elements of an array
+ * @size: the size of each element
+ * Description: 'Program uses *_calloc function'
+ * Return: pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+if (nmemb == 0 || size == 0)
+{
+return (NULL);
+}
+
+*ptr = malloc(nmemb * size);
+
+if (ptr == NULL)
+{
+return (NULL);
+}
+memset(ptr, 0, nmemb * size);
+
+return (ptr);
+}
