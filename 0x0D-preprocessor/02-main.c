05dd5542818592a976df57db6388646b16e28c69
MariamOU <marymyara@gmail.com>
2023-09-11 16:39:15 -0700

    modification of the file 2-main.c

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ * Description: 'Program uses main function'
+ * Return: the name of the file compiled from
+ */
+int main(void)
+{
+printf("%s \n", __FILE__);
+return (0);
+}
