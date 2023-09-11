8fef8bcca1842bbad7cc3ad9c44d865477cb3e34
MariamOU <marymyara@gmail.com>
2023-09-11 08:51:33 -0700

    The file 2-main.c that show the name of the file compiled from

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -4,8 +4,7 @@
  * Description: 'Program uses main function'
  * Return: the name of the file compiled from
  */
-int main()
+int main(void)
 {
 printf("Compiled from file: %s \n", __FILE__);
-return (0);
 }
