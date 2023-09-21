86b74d4b6d899938f6dd9ab69ad46c03ed883521
MariamOU <marymyara@gmail.com>
2023-09-21 05:37:35 -0700

    The assembly file 101-hello_holberton.asm

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * printBeforeMain - The helper function
+ * Description: program uses the function printBeforeMain
+ * Return:nothing
+*/
+void printBeforeMain()
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
+
+/**
+ * before_main - The function
+ * Description: program uses the function before_main
+ * Return: nothing
+*/
+void before_main()
+{
+printBeforeMain();
+}
---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,17 @@
+section .data
+    hello_message db "Hello, Holberton",10,0
+
+section .text
+    global main
+
+extern printf
+
+main:
+    push rbp
+    mov rdi, hello_message
+    call printf
+    pop rbp
+
+    mov rax, 60
+    xor rdi, rdi
+    syscall
