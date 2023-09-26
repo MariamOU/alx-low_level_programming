2fbb178fd88460256d876b1ed044b0c0d316f0f9
MariamOU <marymyara@gmail.com>
2023-09-26 15:35:21 -0700

    The file 0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "lists.h"
+
+
+/**
+ * print_listint - The function
+ * @h: Elements of the list
+ * Description: program uses the function print_listint
+ * Return: The number of nodes
+*/
+
+size_t print_listint(const listint_t *h)
+{
+size_t compteur;
+
+compteur = 0;
+
+while (h != NULL)
+{
+printf("%d\n", h->n);
+compteur++;
+h = h->next;
+}
+
+return (compteur);
+}
