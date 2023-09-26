f7cee789484738cc986932e175d46325ef509119
MariamOU <marymyara@gmail.com>
2023-09-26 16:38:54 -0700

    The file 4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint - The function
+ * @head: The first node of the list
+ * Description: program uses the function free_listint
+ * Return: nothing
+*/
+void free_listint(listint_t *head)
+{
+listint_t *temp;
+
+while (head != NULL)
+{
+temp = head;
+head = head->next;
+free(temp);
+}
+}
