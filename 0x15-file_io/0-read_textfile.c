04cb5ed5c061a257f7b8f41ca51b736d24183614
MariamOU <marymyara@gmail.com>
2023-11-17 14:52:06 -0800

    modif 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,39 +12,33 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-char buffer[BUFFER_SIZE];
-size_t total_read = 0;
-ssize_t readed_bytes;
-ssize_t written_bytes;
+char buf[BUFFER_SIZE];
+size_t total_r = 0;
+ssize_t r_bytes;
+ssize_t wr_bytes;
 ssize_t to_be_written_bytes;
-
+FILE *file;
 
 if (filename == NULL)
 return (0);
-
-FILE *file = fopen(filename, "r");
+file = fopen(filename, "r");
 if (file == NULL)
 return (0);
-
-while ((readed_bytes = fread(buffer, 1, sizeof(buffer), file)) > 0 && total_read < letters)
+while ((r_bytes = fread(buf, 1, sizeof(buf), file)) > 0 && total_r < letters)
 {
-to_be_written_bytes = readed_bytes;
-
-if (total_read + readed_bytes > letters)
+to_be_written_bytes = r_bytes;
+if (total_r + r_bytes > letters)
 {
-to_be_written_bytes = letters - total_read;
+to_be_written_bytes = letters - total_r;
 }
-
-written_bytes = write(STDOUT_FILENO, buffer, to_be_written_bytes);
-if (written_bytes < 0 || written_bytes != to_be_written_bytes)
+wr_bytes = write(STDOUT_FILENO, buf, to_be_written_bytes);
+if (wr_bytes < 0 || wr_bytes != to_be_written_bytes)
 {
 fclose(file);
 return (0);
 }
-
-total_read += readed_bytes;
+total_r += r_bytes;
 }
-
 fclose(file);
-return (total_read);
+return (total_r);
 }
